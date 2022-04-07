872b43b3eea3b3008ece55d9300b96f76ed83a21
AdeshMask <adeshmaske255@gmail.com>
2022-04-07 14:35:59 +0530

    Added:If 50% of win or loose

--- src/com/bridgelabz/Game.java
+++ src/com/bridgelabz/Game.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Game {
 
+    Scanner scanner = new Scanner(System.in);
+
     public void play(double stake, int times, int goal) {
         double tempStake = stake;
         int tempTime = times;
@@ -16,13 +18,34 @@ public class Game {
                     tempStake++;
                     win++;
                     System.out.println("Won: " + tempStake);
+                    if (tempStake == stake /2){
+                        System.out.println("You can resign from the game:\n Press 1 for resign for the day \n press 2 for continue:");
+                        int choice = scanner.nextInt();
+                        if (choice == 1)
+                            break;
+                        else
+                            continue;
+                    }
                 } else {
                     tempStake--;
                     lose++;
                     System.out.println("Loss: " + tempStake);
+                    if (tempStake == stake /2){
+                        System.out.println("You can resign from the game:\n Press 1 for resign for the day \n press 2 for continue:");
+                        int choice = scanner.nextInt();
+                        if (choice == 1)
+                            break;
+                        else
+                            continue;
+                    }
                 }
             }
             else System.out.println("You can't play anymore:");
         }
+        System.out.println("Stake: "+stake);
+        System.out.println("Tempstake:"+tempStake);
+        System.out.println("Win: "+win);
+        System.out.println("Loose: "+lose);
+        System.out.println("times:"+tempTime);
     }
 }
